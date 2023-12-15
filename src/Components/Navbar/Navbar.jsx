a3d4e4f820bcd779f5e4edfb4278c13cc92f1de5
Grzegorz <grzegorztokarz4634@gmail.com>
2023-12-15 01:50:57 +0100

    Adding Routes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import './Navbar.css'
 
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
+import {Link} from "react-router-dom";
 const Navbar = () => {
 
     const [menu, setMenu] = useState("sklep");
@@ -14,14 +15,14 @@ const Navbar = () => {
         <p>ELectronic shop</p>
       </div>
       <ul className='nav-menu'>
-        <li onClick={()=>{setMenu("sklep")}}>Sklep{menu==="sklep"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("telefony")}}>Telefony{menu==="telefony"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("komputery")}}>Komputery{menu==="komputery"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("AGD")}}>AGD{menu==="AGD"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("sklep")}}><Link to='/'>Sklep</Link>{menu==="sklep"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("telefony")}}><Link to='/telefony'>Telefony</Link>{menu==="telefony"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("komputery")}}><Link to='/komputery'>Komputery</Link>{menu==="komputery"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("AGD")}}><Link to='/agd'>AGD</Link>{menu==="AGD"?<hr/>:<></>}</li>
       </ul>
       <div className='nav-login-cart'>
-        <button>Login</button>
-        <img src={cart_icon} alt='' />
+        <Link to='/login'><button>Login</button></Link>
+        <Link to='/cart'> <img src={cart_icon} alt='' /></Link>
         <div className="nav-cart-count">0</div>
       </div>
     </div>
